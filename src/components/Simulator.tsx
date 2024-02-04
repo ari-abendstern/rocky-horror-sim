95c63020bdb3ca1ed1e38076cb07384aeaf16dd2
ari-abendstern <ari.abendstern@gmx.com>
2024-02-04 19:01:17 +0000

    it's giving simulator component

---
+++ src/components/Simulator.tsx
@@ -0,0 +1,11 @@
+function Simulator() {
+    return (
+    <video width="100%" height="100%" poster="https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png">
+      <source src="../assets/video.mp4"/>
+      Susan Sontag just called. Your browser has terrible taste in camp. Or it doesn't support the video tag. One of the two.
+    </video>
+    )
+  }
+  
+  export default Simulator;
+  
