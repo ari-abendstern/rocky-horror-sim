74caad4bcd7f36fb3704fef89ea9a57273634b5a
ari-abendstern <ari.abendstern@gmx.com>
2024-02-04 21:45:12 +0000

    It's giving all the elements. Is it displaying all the elements? No. But it's giving.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { useState } from "react";
 import Simulator from "./components/simulator";
-import BeginButton from "./components/BeginButton";
-
 
 function App() {
 
-  const [isPlaying, setisPlaying] = useState(false)
-
-
-  return (
-    <>
-      { isPlaying ? <Simulator /> : <BeginButton/> }
-    </>
-  );
+  return <Simulator isPlaying={isPlaying} setIsPlaying={setisPlaying} />;
 }
 
 export default App;
--- src/components/Simulator.tsx
+++ src/components/Simulator.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+import BeginButton from "./BeginButton";
+import { ClipLoader } from "react-spinners";
+
 function Simulator() {
-    return (
-    <video width="100%" height="100%" poster="https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png">
-      <source src="../assets/video.mp4"/>
-      Susan Sontag just called. Your browser has terrible taste in camp. Or it doesn't support the video tag. One of the two.
-    </video>
-    )
-  }
+  type State = "loading" | "preSim" | "playing";
+
+  const [currentState, setCurrentState] = useState<State>("loading");
   
-  export default Simulator;
-  
+  const stateSwitch = (currentState: State) => {
+    switch (currentState) {
+      case "loading":
+        return <ClipLoader color="red" />;
+      case "preSim":
+        return <BeginButton />;
+      case "playing":
+        return (
+          <video
+            width="100%"
+            height="100%"
+            poster="https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png"
+          >
+            <source src="../assets/video.mp4" />
+            Susan Sontag just called. Your browser has terrible taste in camp.
+            Or it doesn't support the video tag. One of the two.
+          </video>
+        );
+    }
+  };
+
+  return <>{stateSwitch(currentState)}</>;
+
+}
+
+export default Simulator;
