6b3352ed858ebde35ad0d9089bdcf2e13afbe8bc
ari-abendstern <ari.abendstern@gmx.com>
2024-02-04 19:01:34 +0000

    it's giving button

---
+++ src/components/BeginButton.tsx
@@ -0,0 +1,7 @@
+function BeginButton() {
+  return (
+    <button className="begin-button">Begin Science-Fiction-Double-Feature-Picture-Show Simulator</button>
+  );
+}
+
+export default BeginButton;
