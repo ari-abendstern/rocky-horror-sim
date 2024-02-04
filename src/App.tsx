776e4ff5f59c6301f894c3f575f41a5a3d271eb0
ari-abendstern <ari.abendstern@gmx.com>
2024-02-04 18:17:57 +0000

    it's giving video embedded in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <></>;
+  return (
+  <>
+  <video width="100%" height="100%" controls>
+    <source src="../assets/video.mp4" />
+  </video>
+  </>
+  )
 }
 
 export default App;
