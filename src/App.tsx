02aa1a544d13fe50d93fa9a51b72ba51aaa2e189
ari-abendstern <ari.abendstern@gmx.com>
2024-02-04 18:20:58 +0000

    it's giving information for users without video tag browsers

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
   <>
   <video width="100%" height="100%" controls>
     <source src="../assets/video.mp4" />
+    Susan Sontag just called. Your browser has terrible taste in camp. Or it doesn't support the video tag. One of the two.
   </video>
   </>
   )
